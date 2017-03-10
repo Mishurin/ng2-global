38211560aff1130cbfbaf0aa49e4cb5c1ebdca7f
igor_mishurin@epam.com <igor_mishurin@epam.com>
2017-03-10 23:52:14 +0100

    Added  Courses mocks.

--- webapp/src/app/courses/courses.component.ts
+++ webapp/src/app/courses/courses.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CourseItem } from '../common/entities/course-item.int';
+import { COURSES } from '../common/entities/course-list';
+
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+    selector: 'app-courses',
+    templateUrl: './courses.component.html',
+    styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+    private courses: CourseItem[];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.courses = COURSES;
+    }
 
 }
