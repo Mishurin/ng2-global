ca02083fb6b94bd2400f3cb0a6017eca9e19800a
igor_mishurin@epam.com <igor_mishurin@epam.com>
2017-03-25 23:10:13 +0100

    Loader generated.

--- webapp/src/app/base/base.module.ts
+++ webapp/src/app/base/base.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { HeaderComponent, LogoComponent, FooterComponent } from './index'
+import { HeaderComponent, LogoComponent, FooterComponent } from './index';
+import { LoaderComponent } from './loader/loader.component'
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    exports: [HeaderComponent, FooterComponent],
-    declarations: [HeaderComponent, FooterComponent, LogoComponent]
+    exports: [HeaderComponent, FooterComponent, LoaderComponent],
+    declarations: [HeaderComponent, FooterComponent, LogoComponent, LoaderComponent]
 })
 export class BaseModule { }
