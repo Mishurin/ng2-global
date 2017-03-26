33c6c5850eeaf8589e465ab1558c1bb8c4857888
igor_mishurin@epam.com <igor_mishurin@epam.com>
2017-03-26 01:26:04 +0100

    Implementation and tests for loader component.

--- webapp/src/app/base/loader/loader.component.spec.ts
+++ webapp/src/app/base/loader/loader.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { LoaderComponent } from './loader.component'
+import { LoaderService } from './loader.service'
 
 describe('LoaderComponent', () => {
     let component: LoaderComponent
     let fixture: ComponentFixture<LoaderComponent>
+    let loaderService: LoaderService
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [LoaderComponent]
+            declarations: [LoaderComponent],
+            providers: [LoaderService]
         })
             .compileComponents()
     }))
@@ -16,10 +19,24 @@ describe('LoaderComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(LoaderComponent)
         component = fixture.componentInstance
+        loaderService = TestBed.get(LoaderService)
         fixture.detectChanges()
     })
 
     it('should create', () => {
         expect(component).toBeTruthy()
+        expect(component.isLoaderHidden).toBeTruthy()
+    })
+
+    it('should set hidden flag to true', () => {
+        component.isLoaderHidden = false
+        loaderService.hide()
+        expect(component.isLoaderHidden).toBeTruthy()
+    })
+
+    it('should set hidden flag to false', () => {
+        component.isLoaderHidden = true
+        loaderService.show()
+        expect(component.isLoaderHidden).toBeFalsy()
     })
 })
--- webapp/src/app/base/loader/loader.component.ts
+++ webapp/src/app/base/loader/loader.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core'
 
+import { LoaderService } from './loader.service'
+
 @Component({
-  selector: 'app-loader',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-loader',
+    templateUrl: './loader.component.html',
+    styleUrls: ['./loader.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoaderComponent implements OnInit {
 
-  constructor() { }
+    public isLoaderHidden: boolean = true;
 
-  ngOnInit() {
-  }
+    constructor(private loader: LoaderService) { }
+
+    ngOnInit() {
+        this.loader.getHideLoaderStream().subscribe((isHidden) => this.isLoaderHidden = isHidden)
+    }
 
 }
