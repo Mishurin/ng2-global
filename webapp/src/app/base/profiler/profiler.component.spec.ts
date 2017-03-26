439ce80f277636e37bf0937a2d82ade2b6a5ca83
igor_mishurin@epam.com <igor_mishurin@epam.com>
2017-03-26 04:20:06 +0200

    Added  timespan info and zone subscibtions.

--- webapp/src/app/base/profiler/profiler.component.spec.ts
+++ webapp/src/app/base/profiler/profiler.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ProfilerComponent } from './profiler.component';
+import { ProfilerComponent } from './profiler.component'
 
 describe('ProfilerComponent', () => {
-  let component: ProfilerComponent;
-  let fixture: ComponentFixture<ProfilerComponent>;
+    let component: ProfilerComponent
+    let fixture: ComponentFixture<ProfilerComponent>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProfilerComponent ]
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ProfilerComponent]
+        })
+            .compileComponents()
+    }))
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProfilerComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
     })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProfilerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+})
--- webapp/src/app/base/profiler/profiler.component.ts
+++ webapp/src/app/base/profiler/profiler.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, NgZone, ChangeDetectorRef } from '@angular/core'
 
 @Component({
-  selector: 'app-profiler',
-  templateUrl: './profiler.component.html',
-  styleUrls: ['./profiler.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-profiler',
+    templateUrl: './profiler.component.html',
+    styleUrls: ['./profiler.component.css'],
 })
 export class ProfilerComponent implements OnInit {
 
-  constructor() { }
+    public stable: Date = null
+    public unstable: Date = null
+    public mutable: Object = null;
 
-  ngOnInit() {
-  }
+    constructor(private zone: NgZone, private cd: ChangeDetectorRef) {
+
+    }
+
+    ngOnInit() {
+        this.zone.onUnstable.subscribe(() => {
+            this.unstable = new Date()
+            console.log('Unstable:', this.unstable)
+        })
+        this.zone.onStable.subscribe(() => {
+            this.stable = new Date()
+            console.log('Stable:', this.stable)
+        })
+    }
+
+    makeChanges() { }
+
+    getTimeSpan(): number {
+        return this.stable && this.unstable ? (this.stable.getMilliseconds() - this.unstable.getMilliseconds()) : null;
+    }
 
 }
