66f502c65f20a85393ac1e54afaaccaa094f32a8
igor_mishurin@epam.com <igor_mishurin@epam.com>
2017-04-27 19:27:55 +0200

    Added login form attributes, validation, styling.

--- webapp/src/app/login/login.component.ts
+++ webapp/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core'
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core'
 import { Router } from '@angular/router'
+import { FormControl, NgForm } from '@angular/forms'
 import { Subscription } from 'rxjs/Rx'
 
 import { AuthService } from '../common/index'
@@ -16,8 +17,14 @@ export class LoginComponent implements OnInit, OnDestroy {
     private username: string
     private password: string
     public loginSubscription: Subscription
+    public isLoginError: boolean = false
 
-    constructor(private auth: AuthService, private router: Router, private loader: LoaderService) { }
+    constructor(
+        private auth: AuthService, 
+        private router: Router, 
+        private loader: LoaderService,
+        private cd: ChangeDetectorRef
+    ) { }
 
     ngOnInit() {
     }
@@ -26,12 +33,32 @@ export class LoginComponent implements OnInit, OnDestroy {
         this.loginSubscription.unsubscribe()
     }
 
-    login(username: string, password: string) {
+    login(event: any, form: NgForm) {
+        event.preventDefault()
         this.loader.show()
-        this.loginSubscription = this.auth.login(username, password).subscribe(() => {
+        this.loginSubscription = this.auth.login(
+            form.controls['username'].value, 
+            form.controls['password'].value).subscribe(() => {
             this.router.navigate(['/'])
             this.loader.hide()
-        })
+            this.isLoginError = false
+            this.cd.markForCheck()
+        }, this.handleLoginError.bind(this))
+    }
+
+
+    handleLoginError() {
+        this.loader.hide()
+        this.isLoginError = true
+        this.cd.markForCheck()
+    }
+
+    isSubmitButtonDisabled(form: NgForm): boolean {
+        return form.invalid
+    }
+
+    isRequiredErrorShouldBeShown(ctrl: FormControl) {
+        return ctrl.touched && ctrl.invalid && ctrl.errors['required']
     }
 
 }
