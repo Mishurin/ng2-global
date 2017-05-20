6c7b19bbf0f5d25bae31469df019630cc4ce6a1a
igor_mishurin@epam.com <igor_mishurin@epam.com>
2017-05-21 01:59:37 +0200

    Stubbed long clean up error for login component.

--- webapp/src/app/base/breadcrumbs/breadcrumbs.component.spec.ts
+++ webapp/src/app/base/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-import { Router } from '@angular/router'
 import { CommonModule } from '@angular/common'
 import { RouterTestingModule } from '@angular/router/testing'
 import { Component } from '@angular/core'
--- webapp/src/app/login/login.component.ts
+++ webapp/src/app/login/login.component.ts
@@ -36,7 +36,9 @@ export class LoginComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy() {
-        this.loginSubscription.unsubscribe()
+        if(this.loginSubscription) {
+            this.loginSubscription.unsubscribe()
+        }
     }
 
     login(event: any) {
